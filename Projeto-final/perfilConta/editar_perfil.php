08e0f362a27f6fe4328847d8f7bbd359a003acfd
Kelvem Carlos <kelvemcif@gmail.com>
2022-07-30 04:18:14 -0300

    Perfil de conta profissional criado

--- Projeto-final/perfilConta/editar_perfil.php
+++ Projeto-final/perfilConta/editar_perfil.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include_once '../login/conexao.php';
-ob_start()#serve para limpar o buffer e não causar erro.
+ob_start() #serve para limpar o buffer e não causar erro.
 ?>
 
 <!doctype html>
@@ -16,44 +16,55 @@ ob_start()#serve para limpar o buffer e não causar erro.
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Editar Perfil</title>
+    <style type="text/css">
+        body {
+            background-image: url(../images/banner.svg);
+            background-position: center;
+            background-size: cover;
+            min-height: 700px;
+            padding: 20px 0 80px 0;
+
+        }
+    </style>
 </head>
+
 <body>
-    <div class = "container">
-        <div class = "row">
-            <div class= "col">
-                <h1 style="text-align: center">UPDATE</h1>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 style="text-align: center">Editar Perfil</h1>
                 <form action="../CrudPDO/Crud.php" method="POST">
                     <div class="form-group">
                         <label for="nome">Nome: </label>
-                        <input type="text" name  = "nome" class="form-control" placeholder="Digite seu nome" value = "<?=$_SESSION['nome'];?>" require>
+                        <input type="text" name="nome" class="form-control" placeholder="Digite seu nome" value="<?= $_SESSION['nome']; ?>" require>
                     </div>
 
                     <div class="form-group">
                         <label for="email">Email: </label>
-                        <input type="email" name = "email" class="form-control" placeholder="exemplo: fulano@gmail.com" value = "<?=$_SESSION['email']?>"require>
+                        <input type="email" name="email" class="form-control" placeholder="exemplo: fulano@gmail.com" value="<?= $_SESSION['email'] ?>" require>
                     </div>
 
                     <div class="form-group">
                         <label for="endereço">Endereço: </label>
-                        <input type="text" name = "endereco" class="form-control" placeholder="Seu endereço e n°" value = "<?=$_SESSION['endereco']?>"require>
+                        <input type="text" name="endereco" class="form-control" placeholder="Seu endereço e n°" value="<?= $_SESSION['endereco'] ?>" require>
                     </div>
 
                     <div class="form-group">
                         <label for="telefone">Telefone </label>
-                        <input type="text" name = "telefone" class="form-control" placeholder="Seu endereço e n°" value = "<?=$_SESSION['telefone']?>"require>
+                        <input type="text" name="telefone" class="form-control" placeholder="Seu endereço e n°" value="<?= $_SESSION['telefone'] ?>" require>
                     </div>
-                    <br/>
+                    <br />
 
-                    <div class ="form-group">
-                    <input type = "hidden" name = "id" value="<?=$_SESSION['id'];?>">
-                    </div>
-                    
                     <div class="form-group">
-                        <input type="submit" class = "btn btn-primary"value="Atualizar">
+                        <input type="hidden" name="id" value="<?= $_SESSION['id']; ?>">
+                    </div>
+
+                    <div style="display: inline-block;" class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Atualizar">
                         <input type="hidden" name="OP" value="editarCadastro">
                     </div>
-                    <br/>
-                    <a  class = "btn btn-primary" href="../index.php">voltar para início</a>
+                    <a class="btn btn-primary" href="../index.php">voltar para início</a>
                 </form>
             </div>
         </div>
@@ -69,6 +80,6 @@ ob_start()#serve para limpar o buffer e não causar erro.
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
 
 </html>
--- Projeto-final/perfilConta/perfil.php
+++ Projeto-final/perfilConta/perfil.php
@@ -1,48 +1,94 @@
 <?php
 session_start();
 include_once '../login/conexao.php';
-ob_start()#serve para limpar o buffer e não causar erro.
+ob_start() #serve para limpar o buffer e não causar erro.
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <link rel="stylesheet" href="./css/style.css?key=<?php $key = uniqid(md5(rand())); echo $key ?>">
+    <link rel="stylesheet" href="./css/style.css?key=<?php $key = uniqid(md5(rand()));
+                                                        echo $key ?>">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil</title>
+    <style type="text/css">
+        .logo-menu img{
+            position: relative;
+            left: 100px;
+            top: 30px;
+        }
+        .logo-menu{
+            background-image: url(/Projeto-Esthe/Projeto-final/images/banner.svg);
+        }
+    </style>
 </head>
+
 <body>
+    <div class="logo-menu">
+        <a href="/"><img src="../images/logo.svg" /></a>
+    </div>
     <div class="container">
         <div class="profile-box">
             <img src="../images/new/menu.png" class="menu-icon">
             <img src="../images/new/setting.png" class="setting-icon">
             <img src="../images/new/profile-pic.png" class="profile-pic">
             <?php
-            if(isset($_SESSION['nome'])){
+            //nome, email, senha, telefone, celular, 
+            //servico, rua, bairro, numero, cidade, cep, estado
+            if (isset($_SESSION['nome'])) {
                 echo "<h3>";
                 echo $_SESSION["nome"];
                 echo "</h3>";
-                if(isset($_SESSION['telefone'])){
+                if (isset($_SESSION['telefone'])) {
                     echo "<p>Telefone: ";
                     echo $_SESSION["telefone"];
                     echo "</p>";
                 }
-                }if(isset($_SESSION['endereco'])){
-                    echo "<p>Endereço: ";
-                    echo $_SESSION["endereco"];
+                
+                if (isset($_SESSION['celular'])){
+                    echo "<p>Celular: ";
+                    echo $_SESSION["celular"];
                     echo "</p>";
-                }if(isset($_SESSION['cidade'])){
-                    echo "<p>Cidade: ";
-                    echo $_SESSION["cidade"];
+                }
+            }
+            if (isset($_SESSION['endereco'])) {
+                echo "<p>Endereço: ";
+                echo $_SESSION["endereco"];
+                echo "</p>";
+            }
+            if (isset($_SESSION['cidade'])) {
+                echo "<p>Cidade: ";
+                echo $_SESSION["cidade"];
+                echo "</p>";
+            }
+            if (isset($_SESSION['estado'])) {
+                echo "<p>Estado: ";
+                echo $_SESSION["estado"];
+                echo "</p>";
+            }
+            if (isset($_SESSION['servico'])){
+                echo "<p>Serviço: ";
+                echo $_SESSION["servico"];
+                echo "</p>";
+            }
+            if (isset($_SESSION['rua'])){
+                echo "<p>Rua: ";
+                echo $_SESSION["rua"];
+                echo "</p>";
+                if (isset($_SESSION['numero'])){
+                    echo "<p>N°: ";
+                    echo $_SESSION["numero"];
                     echo "</p>";
-                }if(isset($_SESSION['estado'])){
-                    echo "<p>Estado: ";
-                    echo $_SESSION["estado"];
-                    echo "</p>";
-             }
+                }
+            }
             
+
+
+            
+
             ?>
             <div class="social-media">
                 <img src="../images/new/instagram.png">
@@ -50,14 +96,15 @@ ob_start()#serve para limpar o buffer e não causar erro.
                 <img src="../images/new/dribble.png">
             </div>
             <form action="../CrudPDO/editar.php" method="GET">
-                <a href = "../perfilConta/editar_perfil.php" type="submit">Editar</a>
+                <a href="../perfilConta/editar_perfil.php" type="submit">Editar</a>
             </form>
             <div class="profile-bottom">
                 <p>Caso queira deletar sua conta..</p>
-                <a href="../CrudPDO/excluir.php?id=<?=$_SESSION['id']?>"><img src="../images/new/arrow.png" ></a>
+                <a href="../CrudPDO/excluir.php?id=<?= $_SESSION['id'] ?>"><img src="../images/new/arrow.png"></a>
             </div>
         </div>
 
     </div>
 </body>
+
 </html>
